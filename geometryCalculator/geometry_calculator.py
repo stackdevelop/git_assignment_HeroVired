0a0e10cec1bd33b4383ae351b045ebe35b761016
rahul narayanan <rahul.narayanan@proscope.cloud>
2024-12-11 14:08:16 +0000

    Stashed and retrieved using index and updated the functionality or cirlcle area

--- geometryCalculator/geometry_calculator.py
+++ geometryCalculator/geometry_calculator.py
@@ -10,9 +10,8 @@ class GeometryCalculator:
 if __name__ == "__main__":
     calculator = GeometryCalculator()
 
-    # TODO: Implement the feature to calculate the area of a circle
-    # radius = 5
-    # print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
+    radius = 5
+    print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
 
     # TODO: Implement the feature to calculate the area of a rectangle
     # length = 10
